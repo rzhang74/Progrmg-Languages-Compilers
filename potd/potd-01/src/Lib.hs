eaaf22056d1965125973daacb3acd73b043c69fe
rzhang74 <rzhang74@illinois.edu>
2018-01-20 00:08:50 -0600

    adding lib.hs

--- potd/potd-01/src/Lib.hs
+++ potd/potd-01/src/Lib.hs
@@ -3,4 +3,21 @@ module Lib
     ) where
 
 sumNaturals :: Int -> Int
-sumNaturals = undefined
+{-
+sumNaturals 0 = 0
+sumNaturals n = n + sumNaturals (n - 1) 
+--recursion
+-}
+
+{-
+sumNaturals n = aux n 0
+ where aux 0 t = t
+       aux n t = aux (n-1) (n+t)
+--tail recursion
+-}
+
+{-
+sumNaturals n = sum [1..n]
+-}
+
+sumNaturals n = ((n+1)*n `div` 2)
