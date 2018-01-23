5525446880a0db1dcbfdb383ae60b879a1e911e3
rzhang74 <rzhang74@illinois.edu>
2018-01-23 17:50:18 -0600

    submit potd 2

--- potd/potd-02/src/Lib.hs
+++ potd/potd-02/src/Lib.hs
@@ -8,7 +8,11 @@ import Prelude hiding (signum)
 -- Your code here!
 
 signum :: Int -> Int
-signum = undefined
+signum num
+      | (num == 0) = 0
+      | (num > 0) = 1
+      | (num < 0) = -1
 
 if' :: Bool -> a -> a -> a
-if' = undefined
+if' True x _ = x
+if' False _ y = y
