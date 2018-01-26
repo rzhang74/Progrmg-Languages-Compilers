69725fa4cf034fd3174b81c073e3317784cc9014
Mattox Beckman <mattox@illinois.edu>
2018-01-26 13:06:44 -0600

    potd3

---
+++ potd/potd-03/src/Lib.hs
@@ -0,0 +1,13 @@
+module Lib
+    ( toDigits
+    , doubleEveryOther
+    ) where
+
+-- Your code here!
+-- Remember that you are always free to write helper functions.
+
+toDigits :: Integer -> [Integer]
+toDigits = undefined
+
+doubleEveryOther :: [Integer] -> [Integer] 
+doubleEveryOther = undefined
