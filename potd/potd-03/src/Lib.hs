928e3c2c7841386cad7fb1eea18ec7c2d8552762
rzhang74 <rzhang74@illinois.edu>
2018-01-26 19:47:26 -0600

    potd-03

--- potd/potd-03/src/Lib.hs
+++ potd/potd-03/src/Lib.hs
@@ -7,7 +7,16 @@ module Lib
 -- Remember that you are always free to write helper functions.
 
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits 0 = []
+toDigits x = toDigits (quot x 10) ++ [mod x 10]
 
 doubleEveryOther :: [Integer] -> [Integer] 
-doubleEveryOther = undefined
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [x]
+doubleEveryOther all@(x:y:xs) | mod (len all) 2 == 0 = x*2 : y : doubleEveryOther xs
+                              | otherwise = x : y*2 : doubleEveryOther xs
+
+len :: [Integer] -> Integer
+len [] = 0
+len (x:xs) = 1 + len xs
+
