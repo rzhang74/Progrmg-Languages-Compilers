18e67860263a30184e5537b0abddb6ef75c9b937
Mattox Beckman <mattox@illinois.edu>
2018-02-12 13:52:56 -0600

    POTD 5

---
+++ potd/potd-05/src/Lib.hs
@@ -0,0 +1,16 @@
+module Lib
+    ( split
+    , merge
+    , mergesort
+    ) where
+
+-- Your code here!
+
+split :: Ord a => [a] -> ([a],[a])
+split = undefined
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge = undefined
+
+mergesort :: Ord a => [a] -> [a]
+mergesort = undefined
