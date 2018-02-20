8c902308b6963db7c678d50f7600c6f0ff7bf3fa
Mattox Beckman <mattox@illinois.edu>
2018-02-19 21:19:16 -0600

    Problem of the day 6

---
+++ potd/potd-06/src/Lib.hs
@@ -0,0 +1,16 @@
+module Lib
+    ( (\/)
+    , (/\)
+    , x
+    ) where
+
+-- Your code here!
+
+(\/) :: Ord a => [a] -> [a] -> [a]
+
+
+(/\) :: Ord a => [a] -> [a] -> [a]
+
+
+x :: [a] -> [b] -> [(a, b)]
+
