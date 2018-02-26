51d0878a3ccdeb64bbf71a2c927c1b6a549b4a68
Mattox Beckman <mattox@illinois.edu>
2018-02-26 17:45:26 -0600

    New src directory

---
+++ mps/mp3-cps/src/Lib.hs
@@ -0,0 +1,95 @@
+--- Given Code
+--- ==========
+
+module Lib where
+
+import System.IO (hPutStrLn, hPutStr, stdout, hFlush)
+
+import Data.List (intercalate)
+
+import Data.Functor.Identity (Identity)
+import Text.ParserCombinators.Parsec hiding (Parser)
+import Text.Parsec.Prim (ParsecT)
+
+--- The Types
+--- ---------
+
+data Stmt = Decl String [String] Exp
+            deriving (Eq)
+
+instance Show Stmt where
+    show (Decl f params exp) = f ++ " " ++ intercalate " " params ++ " = " ++ (show exp)
+
+data Exp = IntExp Integer
+         | VarExp String
+         | LamExp String Exp
+         | IfExp Exp Exp Exp
+         | OpExp String Exp Exp
+         | AppExp Exp Exp
+         deriving (Eq)
+
+instance Show Exp where
+    show (VarExp s)       = s
+    show (IntExp i)       = show i
+    show (LamExp x e)     = "(\\" ++ x ++ " -> " ++ (show e) ++ ")"
+    show (IfExp e1 e2 e3) = "(if " ++ show e1 ++ " then " ++ show e2
+                            ++ " else " ++ show e3 ++ ")"
+    show (OpExp op e1 e2) = "(" ++ show e1 ++ " " ++ op ++ " " ++ show e2 ++ ")"
+    show (AppExp f e)     = show f ++ " " ++ show e
+
+ctorShow :: Exp -> String
+ctorShow (VarExp s)       = "VarExp " ++ show s
+ctorShow (IntExp i)       = "IntExp " ++ show i
+ctorShow (LamExp x e)     = "LamExp " ++ show x ++ " (" ++ ctorShow e ++ ")"
+ctorShow (IfExp e1 e2 e3) = "IfExp (" ++ ctorShow e1 ++ ") ("
+                                ++ ctorShow e2 ++ ") ("
+                                ++ ctorShow e3 ++ ")"
+ctorShow (OpExp op e1 e2) = "OpExp " ++ show op ++ " ("
+                                ++ ctorShow e1 ++ ") ("
+                                ++ ctorShow e2 ++ ")"
+ctorShow (AppExp f e)     = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ ")"
+
+--- Problems
+--- ========
+
+--- Manual Translation
+--- ------------------
+
+--- ### `factk :: Integer -> (Integer -> t) -> t`
+
+factk :: Integer -> (Integer -> t) -> t
+factk = undefined
+
+--- ### `evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t`
+
+evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t
+evenoddk = undefined
+
+--- Automated Translation
+--- ---------------------
+
+gensym :: Integer -> (String, Integer)
+gensym i = ("v" ++ show i, i + 1)
+
+--- ### Define `isSimple`
+
+isSimple :: Exp -> Bool
+isSimple = undefined
+
+--- ### Define `cpsExp` - Overview
+
+cpsExp :: Exp -> Exp -> Integer -> (Exp, Integer)
+cpsExp = undefined
+
+--- #### Define `cpsExp` for Integer and Variable Expressions
+
+--- #### Define `cpsExp` for Application Expressions
+
+--- #### Define `cpsExp` for Operator Expressions
+
+--- #### Define `cpsExp` for If Expressions
+
+--- ### Define `cpsDecl`
+
+cpsDecl :: Stmt -> Stmt
+cpsDecl = undefined
